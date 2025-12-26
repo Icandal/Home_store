75e75150892c3306014757f40389e828d0eb2e22
KebabRach <igorkandalov95@gmail.com>
2025-12-26 11:09:02 +0300

    Pagination added

--- goods/views.py
+++ goods/views.py
@@ -1,20 +1,34 @@
+from unicodedata import category
+from django.core.paginator import Paginator
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
 
 
-def catalog(request) -> HttpResponse:
+def catalog(request, category_slug, page=1) -> HttpResponse:
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
         }
     return render(request=request, template_name="goods/catalog.html", context=context)
 
 
-def product(request) -> HttpResponse:
+def product(request, product_slug) -> HttpResponse:
+    product = Products.objects.get(slug=product_slug)
 
-    return render(request=request, template_name="goods/product.html")
+    context = {
+        "product" : product
+    } 
+
+    return render(request=request, template_name="goods/product.html", context=context)
